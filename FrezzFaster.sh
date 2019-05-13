26f46c042b223bfb26a12b9a4926de18038558ed
Frezz <49801480+FrezzGans@users.noreply.github.com>
2019-05-13 16:29:19 +0700

    FrezzGans
    #NoRecode:v
    #EzCoding:v

---
+++ FrezzFaster.sh
@@ -0,0 +1,142 @@
+#!/system/xbin/bash
+#RecodeGabaklBikinLuPro:v
+#CapeGeblek:v
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+echo $green "Laoding......."
+sleep 0.8
+clear
+echo $green "Laoding......."
+sleep 0.8
+clear
+echo $green "Laoding......."
+sleep 0.8
+clear
+echo $green "Laoding......."
+sleep 0.8
+clear
+echo $green "Laoding......."
+sleep 0.8
+clear
+echo $green "Laoding......."
+sleep 0.8
+clear
+echo $green "Laoding......."
+sleep 0.8
+clear
+echo $red "FrezzGans(Wkwkkwk)"
+sleep 3
+clear
+echo $white "ToolsSedangDalamproses^_^"
+sleep 1
+echo $purple "Tunggu 10 Detik:v"
+sleep 5
+clear
+echo $green "Sabar aja kayak nunggu chat doi:v"
+sleep 5
+clear
+echo $indigo "============================"
+echo $red "#Creator : Frezz"
+echo $blue "#Team : K CROWN"
+echo $purple "#Whatsapp : 089633462×××"
+echo $cyan "#Email : frezzgans@gmail.com"
+echo $green "============================"
+echo " "
+echo $red "[FrezzGans]--[ToolsMempercepatJaringan]"
+echo $purple "[+]=================================[+]"
+echo $yellow "[1]»Mempercepat Jaringan Ver1"
+echo $indigo "[2]»Mempercepat Jaringan Ver2"
+echo $green "[3]»Mempercepat Jaringan Ver3"
+echo $cyan "[4]»Mempercepat Jaringab Ver4"
+echo $blue "[5]»Mempercepat Jaringan Ver5"
+echo "\033[35;1m"
+read -p "@»[FrezzTools]#" f
+
+if [ $f = 1 ]
+then 
+clear 
+figlet "F R E Z Z" |lolcat
+sleep 2
+echo $green "Memproses..."
+sleep 2
+echo $red "[DONE]"
+sleep 1
+echo $blue "====FREZZ===="
+echo " "
+ping 8.8.8.8
+fi
+
+if [ $f = 2 ]
+then 
+clear 
+figlet "F R E Z Z" |lolcat
+sleep 2
+echo $green "Memproses..."
+sleep 2
+echo $red "[DONE]"
+sleep 1
+echo $blue "====FREZZ===="
+echo " "
+ping -D www.google.com
+fi
+
+if [ $f = 3 ]
+then 
+clear 
+figlet "F R E Z Z" |lolcat
+sleep 2
+echo $green "Memproses..."
+sleep 2
+echo $red "[DONE]"
+sleep 1
+echo $blue "====FREZZ===="
+echo " "
+ping 1.1.1.1
+fi
+
+if [ $f = 4 ]
+then 
+clear 
+figlet "F R E Z Z" |lolcat
+sleep 2
+echo $green "Memproses..."
+sleep 2
+echo $red "[DONE]"
+sleep 1
+echo $blue "====FREZZ===="
+echo " "
+ping 9.9.9.9
+fi
+
+if [ $f = 5 ]
+then 
+clear 
+figlet "F R E Z Z" |lolcat
+sleep 2
+echo $green "Memproses..."
+sleep 2
+echo $red "[DONE]"
+sleep 1
+echo $blue "====FREZZ===="
+echo " "
+ping -D www.firefox.com
+fi
+
+if [ $f = 00 ]
+then 
+clear 
+figlet "F R E Z Z" |lolcat
+sleep 2
+echo $green "FrezzGans"
+sleep 2
+echo $red "Thangks To : K Crown"
+echo $indigo "Thangks To : Finzert,4ŠÇ3,Mr.₱01,NanoSyntax,AndAllMember[K^Ç]"
+sleep 1
+echo $blue "====FREZZ===="
+echo " "
+fi
